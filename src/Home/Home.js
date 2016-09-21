ea5d5e1aaa4dc1e8c383196cd43d68d3efdd0d6b
newming <a977527479@sina.com>
2016-09-21 11:48:45 +0800

    home

--- src/Home/Home.js
+++ src/Home/Home.js
@@ -3,17 +3,11 @@ import React from 'react'
 class Home extends React.Component {
   render () {
     return(
-      <div>
-        <p>sfdsfds</p>
-        <p>sfdsfds</p>
-        <p>sfdsfds</p>
-        <p>sfdsfds</p>
-        <p>sfdsfds</p>
-        <p>sfdsfds</p>
-        <p>sfdsfds</p>
-        <p>sfdsfds</p>
-        <p>sfdsfds</p>
-
+      <div className="home-container">
+        <div className="home-cover"></div>
+        <h3>HI, I'M NEWMING</h3>
+        <p>WEB DEVELOPER</p>
+        <button>HIRE ME</button>
       </div>
     )
   }
