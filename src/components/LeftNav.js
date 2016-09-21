70c7f791b0e05db165685a1c734c09f96362e78a
newming <a977527479@sina.com>
2016-09-21 16:52:53 +0800

    simple context

--- src/components/LeftNav.js
+++ src/components/LeftNav.js
@@ -2,28 +2,10 @@ import React, { PropTypes } from 'react';
 import { Link } from 'react-router';
 
 class LeftNav extends React.Component {
-  constructor(){
-    super();
-    title:'Home'
-  }
-  componentWillReceiveProps(){
-    this.setTitle();
-  }
-  componentWillMount(){
-    this.setTitle();
-  }
-  setTitle(){
-    this.setState({
-      title: this.context.router.isActive('/',true) ? 'Home' :
-              this.context.router.isActive('/blog') ? 'Blog' :
-              this.context.router.isActive('/work') ? 'Work' :
-              this.context.router.isActive('/about') ? 'About' : 'Item'
-    })
-  }
   render () {
     return(
       <div className="left-nav">
-        <h3>Newming@{this.state.title}</h3>
+        <h3>Newming@{this.props.title}</h3>
         <Link to="/" activeStyle={{backgroundColor:'rgba(0,0,0,0.3)'}} onlyActiveOnIndex={true}><span className="glyphicon glyphicon-home" aria-hidden="true"></span>&nbsp;&nbsp;Home</Link>
         <Link to="blog" activeStyle={{backgroundColor:'rgba(0,0,0,0.3)'}}><span className="glyphicon glyphicon-book" aria-hidden="true"></span>&nbsp;&nbsp;Blog</Link>
         <Link to="work" activeStyle={{backgroundColor:'rgba(0,0,0,0.3)'}}><span className="glyphicon glyphicon-list-alt" aria-hidden="true"></span>&nbsp;&nbsp;Work</Link>
@@ -31,10 +13,6 @@ class LeftNav extends React.Component {
       </div>
     )
   }
-}
-
-LeftNav.contextTypes = {
-  router: React.PropTypes.object.isRequired
 }
 
 export default LeftNav;
