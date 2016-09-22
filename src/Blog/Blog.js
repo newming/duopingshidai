6eb350da5a27221dde8250254e8a0757ee661a9b
newming <a977527479@sina.com>
2016-09-22 14:09:57 +0800

    marked

--- src/Blog/Blog.js
+++ src/Blog/Blog.js
@@ -3,13 +3,6 @@ import React from 'react';
 import BlogCard from '../components/BlogCard';
 import { getJson } from '../utils/helpers';
 
-let data=[
-  {index:1,title:'如何成为高手',desc:'天天学天天学天天学天天学天天学天天学天天学天天学',url:'a'},
-  {index:2,title:'如何成为高手',desc:'天天学天天学天天学天天学天天学天天学天天学天天学',url:'b'},
-  {index:3,title:'如何成为高手',desc:'天天学天天学天天学天天学天天学天天学天天学天天学',url:'c'},
-  {index:4,title:'如何成为高手',desc:'天天学天天学天天学天天学天天学天天学天天学天天学',url:'d'}
-]
-
 class Blog extends React.Component {
   constructor(){
     super();
@@ -30,7 +23,7 @@ class Blog extends React.Component {
   render () {
     return(
       <div style={{width:'100%',marginTop:'20px'}}>
-        { this.state.data.map( (item,i) => <BlogCard {...item} key={i} /> ) }
+        { this.state.wait ? '请稍等' : this.state.data.map( (item,i) => <BlogCard {...item} key={i} /> ) }
       </div>
     )
   }
